9d46eeed4341d3943900be4173da87f78a42e5f8
wangjian <wj985712588@gmail.com>
2020-08-19 02:04:01 +0800

    首次提交

---
+++ easy-web/src/main/java/cn/mhonor/EasyWebApplication.java
@@ -0,0 +1,15 @@
+package cn.mhonor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mr.mhonor
+ * @date 2019/8/20 23:46
+ */
+@SpringBootApplication
+public class EasyWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyWebApplication.class, args);
+    }
+}
